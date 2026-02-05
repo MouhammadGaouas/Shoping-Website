9bc4e3f7463d69f7a08b9e6db340e3eb54173091
MouhammadGaouas <gaouasmouhammad@gmail.com>
2026-02-05 08:06:09 +0100

    nothing new

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react"
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-8xl italic tracking-widest">Booking <span className="text-blue-500">Project</span></h1>
+      <h1 className="text-8xl italic tracking-widest">Shoping<span className="text-blue-500"> Website</span></h1>
     </main>
   )
 }
