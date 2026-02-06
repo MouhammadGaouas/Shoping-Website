35353ebe275cd77c64fb43932743cbae6a6a4086
MouhammadGaouas <gaouasmouhammad@gmail.com>
2026-02-06 10:04:13 +0100

    initialize DB Schema

---
+++ api.md
@@ -0,0 +1,6 @@
+# end points 
+
+login -> /api/auth/login
+register -> /api/auth/register
+create order -> /api/order/create
+delete order -> /api/order/delete
--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,39 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcrypt";
-import { error } from "console";
 
 export async function POST(request: NextRequest) {
   const { email, password } = await request.json();
 
-  console.log(email, password);
-  if (!email || !password) return NextResponse.json({ message: "Invalide Ts" });
+
+  if (!email || !password) return NextResponse.json({ message: "Invalide input" });
 
   try {
-    console.log(0);
 
-    const isExist = await prisma.user.findUnique({
-      where: { email },
-    });
-
-    console.log(1);
-    if (isExist)
-      return NextResponse.json(
-        { message: "Account already exist" },
-        { status: 409 },
-      );
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await prisma.user.create({
-      data: {
-        email: email,
-        password: hashedPassword,
-      },
-    });
   } catch (err) {
-    console.error("REGISTER ERROR:", err);
-    return NextResponse.json({ message: "internal server error" });
+
   }
-  return NextResponse.json({ message: "Account created succefully." });
 }
