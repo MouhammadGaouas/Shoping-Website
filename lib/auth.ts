066d82c2856b61878cd88b34c62973421add878e
MouhammadGaouas <gaouasmouhammad@gmail.com>
2026-02-10 08:58:19 +0100

    new updates

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,18 +1,21 @@
 import { NextResponse, NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
-export function validateAuthCookies(request: NextRequest) {
+export function validateAuthUser(request: NextRequest) {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET environment variable is not mising");
+  }
+
   try {
     const token = request.cookies.get("token")?.value;
 
-    if (!token)
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    if (!token) return null;
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
 
     return decoded;
   } catch (err) {
     console.error("Invalide token : ", err);
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    return null;
   }
 }
