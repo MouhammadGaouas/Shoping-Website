2cd266af58a168c15dd8e15e27b9d1d71002ddfb
MouhammadGaouas <gaouasmouhammad@gmail.com>
2026-03-01 23:18:28 +0100

    update

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,32 +1,13 @@
-import { NextResponse, NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "@/lib/prisma";
 
-export function validateAuthUser(request: NextRequest) {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET environment variable is not mising");
-  }
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
 
-  try {
-    const token = request.cookies.get("token")?.value;
-
-    if (!token) return null;
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-
-    return decoded as {
-      id: string,
-      role: string
+    emailAndPassword: {
+        enabled: true ,
     }
-    
-  } catch (err) {
-    console.error("Invalide token : ", err);
-    return null;
-  }
-}
-
-export function requireRole(user: { role: string }, role: 'USER' | 'SELLER') {
-
-  if (!user.role || user.role !== role) {
-    throw new Error("Forbidden")
-  }
-}
+});
